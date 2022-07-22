e5e72fbc678c1281711e2b3a2357cdf3daeae7fe
Nicole Cevey <nicole.cevey@gmail.com>
2022-07-22 17:14:38 -0400

    Adding project data json file

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,44 +1,63 @@
 import "../Work/Work.scss";
 import docTurn from "../../assets/docturn-image.png";
-import githubIcon from "../../assets/icons/GitHub-Mark.png"; 
-import launchIcon from "../../assets/icons/launch_black_24dp.svg"
+import githubIcon from "../../assets/icons/GitHub-Mark.png";
+import launchIcon from "../../assets/icons/launch_black_24dp.svg";
 import rightChevron from "../../assets/icons/chevron-right.svg";
 import leftChevron from "../../assets/icons/chevron-left.svg";
+import workData from "../../data/workData.json";
 
 export default function Work() {
-    return (
-        <section className="work">
-            <h1 className="work__title">Projects I've Built</h1>
-            <article className="carousel">
-                <img src={leftChevron} className="carousel__next-icon"></img>
-                <div className="carousel__track-container">
-                    <ul className="carousel__track">
+
+    // [currentSlide, setCurrentSlide] = 
+    
+    // const slides = Array.from(track.children);
+    
+  return (
+    <section className="work">
+      <h1 className="work__title">Projects I've Built</h1>
+      <article className="carousel">
+        <div className="carousel__container">
+          <img src={leftChevron} className="carousel__next-icon"></img>
+          <div className="carousel__track-container">
+            <ul className="carousel__track">
+                {workData.map((project, id) => {
+                    console.log(project.stack)
+                    return(
                         <li className="carousel__slide">
                             <img src={docTurn} className="carousel__image"></img>
                             <div className="carousel__description">
-                                <h2 className="carousel__project-title">DocTurn</h2>
-                                <p className="carousel__project-text">A document management application that makes keeping track of documents easy.</p>
-                                <p className="carousel__project-text">Intended for lawyers and other business professionals who turn many documents on a regular basis.</p>
-                                <ul className="carousel__tech-list">
-                                    <li>JavaScript</li>
-                                    <li>React</li>
-                                    <li>Sass</li>
-                                    <li>Firebase</li>
-                                </ul>
-                                <a href="https://github.com/nicolecevey/docturn" target="_blank" alt="Demo of DocTurn App">
-                                    <img src={githubIcon} className="carousel__link-icon">
-                                    </img>
-                                </a>
-                                <a href="https://docturn.vercel.app/" target="_blank">
-                                    <img src={launchIcon} className="carousel__link-icon">
-                                    </img>
-                                </a>
+                            <h2 className="carousel__project-title">{project.name}</h2>
+                            <p className="carousel__project-text">{project.description}</p>
+                            <ul className="carousel__tech-list">
+                                {(project.stack).map((tech) => {
+                                    return (<li>{tech}</li>)
+                                    })
+                                }
+                            </ul>
+                            <a
+                                href={project.github}
+                                target="_blank"
+                                alt="Demo of DocTurn App"
+                            >
+                                <img src={githubIcon} className="carousel__link-icon"></img>
+                            </a>
+                            <a href={project.link} target="_blank">
+                                <img src={launchIcon} className="carousel__link-icon"></img>
+                            </a>
                             </div>
                         </li>
-                    </ul>
-                </div>
-                <img src={rightChevron} className="carousel__next-icon"></img>
-            </article>
-        </section>
-    )
+                    )
+                })}
+            </ul>
+          </div>
+          <img src={rightChevron} className="carousel__next-icon"></img>
+        </div>
+        <div className="carousel__indicators">
+          <button className="carousel__indicator carousel__indicator--current-slide"></button>
+          <button className="carousel__indicator"></button>
+          <button className="carousel__indicator"></button>
+        </div>
+      </article>
+    </section>
+  );
 }

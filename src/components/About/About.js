517abba34b16b58806fc2f341aed6f3b6aedebbe
Nicole Cevey <nicole.cevey@gmail.com>
2022-07-21 23:12:34 -0400

    Adding list of technologies

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,9 +4,32 @@ import "./About.scss";
 export function About() {
  return (
     <article className="about">
-        <img src={headshot} className="about__headshot"></img>
-        <div className="about__text">
-            <h1>About Me</h1>
+        <img 
+            src={headshot} 
+            className="about__headshot" 
+            alt="Headshot"
+        ></img>
+        <div className="about__description">
+            <h1 className="about__title">About Me</h1>
+            <div className="about__text">
+                <p>I'm a former licensed paralegal who worked as a legal assistant at one of Canada's largest corporate law firms before making the transition to web development.</p>
+            </div>
+            <section className="technologies">
+                <h2 className="technologies__title">Some Technologies I use</h2>
+                    <ul className="technologies__list">
+                        <div>
+                            <li>JavaScript</li>
+                            <li>React</li>
+                            <li>Sass</li>
+                            <li>Node.js</li>
+                        </div>
+                        <div>
+                            <li>Express</li>
+                            <li>Firebase</li>
+                            <li>MongoDB</li>
+                        </div>
+                    </ul>
+            </section>
         </div>
     </article>
 

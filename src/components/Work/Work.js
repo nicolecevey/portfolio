fded0e609421f49a44106d307955c01876442e18
Nicole Cevey <nicole.cevey@gmail.com>
2022-07-23 15:50:12 -0400

    Adding BrainFlix data

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,10 +1,10 @@
 import "../Work/Work.scss";
-import docTurn from "../../assets/docturn-image.png";
 import githubIcon from "../../assets/icons/GitHub-Mark.png";
 import launchIcon from "../../assets/icons/launch_black_24dp.svg";
 import rightChevron from "../../assets/icons/chevron-right.svg";
 import leftChevron from "../../assets/icons/chevron-left.svg";
 import workData from "../../data/workData.json";
+import React, { useState } from "react";
 
 export default function Work() {
 
@@ -20,11 +20,11 @@ export default function Work() {
           <img src={leftChevron} className="carousel__next-icon"></img>
           <div className="carousel__track-container">
             <ul className="carousel__track">
-                {workData.map((project, id) => {
+                {workData.map((project, index) => {
                     console.log(project.stack)
                     return(
                         <li className="carousel__slide">
-                            <img src={docTurn} className="carousel__image"></img>
+                            <img src={`http://localhost:3000/Images/project-image${index + 1}.png`} className="carousel__image"></img>
                             <div className="carousel__description">
                             <h2 className="carousel__project-title">{project.name}</h2>
                             <p className="carousel__project-text">{project.description}</p>

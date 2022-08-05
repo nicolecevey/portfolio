8bf1104f2e8d6b22b2da1e6a8681a5ed44e2222f
Nicole Cevey <nicole.cevey@gmail.com>
2022-08-04 22:56:26 -0400

    Changing animation to hero banner text

--- src/components/HeroBanner/HeroBanner.js
+++ src/components/HeroBanner/HeroBanner.js
@@ -3,7 +3,7 @@ import "./HeroBanner.scss";
 export function HeroBanner() {
   return (
     <section className="hero">
-      <div className="hero__text">
+      <div className="hero__text" data-aos="fade-up">
         <h1>Hi, my name is</h1>
         <span className="hero__name">Nicole Cevey.</span>
         <p className="hero__description">

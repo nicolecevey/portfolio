7b4210c80336a9b5dc102e13bee85f278f465b90
Nicole Cevey <nicole.cevey@gmail.com>
2024-01-12 11:41:16 -0500

    Refresh of site. Adding new work

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,65 +6,34 @@ import Aos from "aos";
 import { useEffect } from "react";
 
 export function About() {
-  useEffect(() => {
-    Aos.init({ duration: 600 });
-  }, []);
+	useEffect(() => {
+		Aos.init({ duration: 600 });
+	}, []);
 
-  const handleClick = () => {
-    window.scroll(0, 700);
-  };
+	const handleClick = () => {
+		window.scroll(0, 700);
+	};
 
-  return (
-    <>
-      <a id="about" className="anchor"></a>
-      <img
-        src={downChevron}
-        className="arrow-down"
-        onClick={() => handleClick()}
-      ></img>
-      <article className="about" data-aos="fade-up">
-        <img src={headshot} className="about__headshot" alt="Headshot"></img>
-        <div className="about__description">
-          <h1 className="about__title">About Me</h1>
-          <div className="about__text">
-            <p>
-              I'm a web developer based in Toronto. I recently made the
-              transition from law to web development after working in a
-              corporate law firm as a legal assistant. I'm passionate about
-              developing beautiful apps.
-            </p>
-          </div>
-          <section className="about__knowledge">
-            <div className="technologies">
-              <h2 className="technologies__title">Some Technologies I use</h2>
-              <ul className="technologies__list">
-                <div>
-                  <li>JavaScript</li>
-                  <li>React</li>
-                  <li>Sass</li>
-                  <li>Node.js</li>
-                  <li>Express</li>
-                </div>
-                <div>
-                  <li>SQL</li>
-                  <li>mySQL</li>
-                  <li>Firebase</li>
-                  <li>MongoDB</li>
-                  <li>Figma</li>
-                </div>
-              </ul>
-            </div>
-            <div className="learning">
-              <h2 className="learning__title">What I'm learning</h2>
-              <ul className="learning__list">
-                <li>Web3.js</li>
-                <li>Solidity</li>
-                <li>AWS</li>
-              </ul>
-            </div>
-          </section>
-        </div>
-      </article>
-    </>
-  );
+	return (
+	<>
+		<a id="about" className="anchor"></a>
+		<img
+			src={downChevron}
+			className="arrow-down"
+			onClick={() => handleClick()}
+		></img>
+		<article className="about" data-aos="fade-up">
+			<img src={headshot} className="about__headshot" alt="Headshot"></img>
+			<div className="about__description">
+			<h1 className="about__title">About Me</h1>
+			<div className="about__text">
+				<p>
+				I'm a frontend developer based in Toronto. I made the transition from law to software development in 2021. I'm passionate about
+				developing beautiful apps.
+				</p>
+			</div>
+			</div>
+		</article>
+		</>
+	);
 }
